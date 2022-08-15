55bacc5580ab2155506300e4cd026ae1484b0281
Rob Hawkins <rbt.hawkins@live.com>
2022-08-14 19:41:47 -0500

    toptags()

--- cloud.py
+++ cloud.py
@@ -100,28 +100,29 @@ def toptags():
             linkcount[taglink[tl][0]] = linkcount[taglink[tl][0]] + linkid[taglink[tl][1]][2]
         else:
             linkcount[taglink[tl][0]] = linkid[taglink[tl][1]][2]
-        if linkcount[taglink[tl][0]] > max_count:
-            max_count = linkcount[taglink[tl][0]]
-        if min_count == 0 or linkcount[taglink[tl][0]] < min_count:
-            min_count = linkcount[taglink[tl][0]]
 
+    linkcount = dict(sorted(linkcount.items(), key = itemgetter(1), reverse = True)[:25])
+    for t in linkcount:
+        if t[1] > max_count:
+            max_count = t[1]
+        if t[1] < min_count or min_count == 0:
+            min_count = t[1]
+    
     max_qty = math.log10(max_count)
     min_qty = math.log10(min_count)
 
     size_step = (max_size - min_size) / (max_qty - min_qty + 0.1)
     
     taglist = []
-    numtags = 0
     for id in sorted(tagid.items(), key=lambda kv:kv[1].casefold()):
-        if id[0] in linkcount and numtags < 99:
+        if id[0] in linkcount:
             tag_desc = tagid[id[0]] #f"Tag {i} Description"
             tag_url = id[0] # Tag ID
             tag_colr = f"#11f"
             tag_count = linkcount[id[0]] #linkid[i][2] #f"{4*i}"
             tag_size = int(min_size + (math.log10(linkcount[id[0]]) - min_qty) * size_step) #f"{100+2*id[0]}"
             taglist.append([tag_desc,tag_url,tag_size,tag_colr,tag_count])
-            numtags += 1
-    top_tags = sorted(taglist, key=lambda v:v[4], reverse=True)[0:25]
 
-    return sorted(top_tags, key=lambda v:v[0])
-
+    #top_tags = sorted(taglist, key=lambda v:v[4], reverse=True)[0:25]
+    #return sorted(top_tags, key=lambda v:v[0])
+    return taglist
