c11ac72056b98ad546c987031c34d7b6718f9594
Rob Hawkins <rbt.hawkins@live.com>
2022-08-16 15:21:59 -0500

    Preparing for Gunicorn deployment

--- app.py
+++ app.py
@@ -14,56 +14,56 @@ def create_app(testing: bool = True):
     app.config["SESSION_TYPE"] = "filesystem"
     Session(app)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    if session.get("name"):
-        name = session.get("name")
-        return render_template('home.html', page_title='PyCloud Menu', page_heading='The PyCloud', username=name, tag_list=cloud.tags(25), link_list=cloud.toplinks(25))
-    else:
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        if session.get("name"):
+            name = session.get("name")
+            return render_template('home.html', page_title='PyCloud Menu', page_heading='The PyCloud', username=name, tag_list=cloud.tags(25), link_list=cloud.toplinks(25))
+        else:
+            return redirect("/login")
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html', page_title='PyCloud Login', page_heading='PyCloud')
+
+    @app.route('/dologin', methods=['POST','GET'])
+    def dologin():
+        if request.method == "POST":
+            email = request.form.get("email")
+            passwd = request.form.get("passwd")
+            db = sql_data.create_connection('pycloud.db')
+            if db:
+                sql = "SELECT email, passwd, name FROM menu_user"
+                cursor = db.execute(sql)
+                for row in cursor:
+                    if email==row[0] and passwd==row[1]:
+                        session["name"] = row[2]
+                db.close()
+        return redirect("/index")
+
+    @app.route('/add/<int:n1>/<int:n2>/')
+    def add(n1, n2):
+        return '<h1>{}</h1>'.format(n1 + n2)
+
+    @app.route('/tags')
+    def tags():
+        return render_template('tags.html', page_title='PyCloud Menu', page_heading='PyCloud', tag_list=cloud.tags(-1))
+
+    @app.route('/links/<int:tagid>')
+    def links(tagid):
+        return render_template('links.html', page_title='PyCloud Menu', page_heading='PyCloud', tag_name=tag.get_name(tagid), link_list=cloud.links(tagid))
+
+    @app.route('/alllinks')
+    def alllinks():
+        return render_template('links.html', page_title='PyCloud Menu', page_heading='PyCloud', tag_name="All Links", link_list=cloud.links(0))
+
+    @app.route('/logout')
+    def logout():
+        session["name"] = None
         return redirect("/login")
 
-@app.route('/login')
-def login():
-    return render_template('login.html', page_title='PyCloud Login', page_heading='PyCloud')
-
-@app.route('/dologin', methods=['POST','GET'])
-def dologin():
-    if request.method == "POST":
-        email = request.form.get("email")
-        passwd = request.form.get("passwd")
-        db = sql_data.create_connection('pycloud.db')
-        if db:
-            sql = "SELECT email, passwd, name FROM menu_user"
-            cursor = db.execute(sql)
-            for row in cursor:
-                if email==row[0] and passwd==row[1]:
-                    session["name"] = row[2]
-            db.close()
-    return redirect("/index")
-
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
-
-@app.route('/tags')
-def tags():
-    return render_template('tags.html', page_title='PyCloud Menu', page_heading='PyCloud', tag_list=cloud.tags(-1))
-
-@app.route('/links/<int:tagid>')
-def links(tagid):
-    return render_template('links.html', page_title='PyCloud Menu', page_heading='PyCloud', tag_name=tag.get_name(tagid), link_list=cloud.links(tagid))
-
-@app.route('/alllinks')
-def alllinks():
-    return render_template('links.html', page_title='PyCloud Menu', page_heading='PyCloud', tag_name="All Links", link_list=cloud.links(0))
-
-@app.route('/logout')
-def logout():
-    session["name"] = None
-    return redirect("/login")
-
-@app.route('/linkcount/<int:link_id>')
-def linkcount(link_id):
-    clicks = link.incr_clicks(link_id)
-    return redirect(request.referrer)
+    @app.route('/linkcount/<int:link_id>')
+    def linkcount(link_id):
+        clicks = link.incr_clicks(link_id)
+        return redirect(request.referrer)
