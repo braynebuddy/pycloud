8b6c13f0d0abe1748315bdf8f9fb86d787a736e1
Rob Hawkins <rbt.hawkins@live.com>
2022-08-16 15:07:57 -0500

    Prepare app.py for Gunicorn deployment

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ import tag
 import link
 import cloud
 
-app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+def create_app(testing: bool = True):
+    app = Flask(__name__)
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
 @app.route('/')
 @app.route('/index')
