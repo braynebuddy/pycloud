7fde9d5c31c3e629316805a9f77f3adb3070ff0c
Rob Hawkins <braynebuddy@gmail.com>
2022-08-24 11:08:54 -0500

    Adding Search in Javascript

---
+++ static/bin/search.js
@@ -0,0 +1,27 @@
+// Search Function
+//
+// from - https://www.geeksforgeeks.org/live-search-using-flask-and-jquery/
+//
+jQuery(document).ready(function($){
+  
+    $('.tagcloud li').each(function(){
+    $(this).attr('search-term', $(this).text().toLowerCase());
+    });
+      
+    $('.search-box').on('keyup', function(){
+      
+    var searchTerm = $(this).val().toLowerCase();
+      
+        $('.tagcloud li').each(function(){
+      
+            if ($(this).filter('[search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+      
+        });
+      
+    });
+      
+    });
