b10f65bf2170df81626f1e20208ee49864b1c760
Rob Hawkins <braynebuddy@gmail.com>
2022-08-24 11:37:06 -0500

    Adding Search in Javascript

--- static/bin/search.js
+++ static/bin/search.js
@@ -5,15 +5,22 @@
 jQuery(document).ready(function($){
   
     $('.tagcloud li').each(function(){
-    $(this).attr('search-term', $(this).text().toLowerCase());
+        $(this).attr('search-term', $(this).text().toLowerCase());
+    });
+    $('.search-list p').each(function(){
+        $(this).attr('search-term', $(this).text().toLowerCase());
     });
       
     $('.search-box').on('keyup', function(){
-      
-    var searchTerm = $(this).val().toLowerCase();
-      
+        var searchTerm = $(this).val().toLowerCase();
         $('.tagcloud li').each(function(){
-      
+            if ($(this).filter('[search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+        $('.search-list p').each(function(){
             if ($(this).filter('[search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
                 $(this).show();
             } else {
